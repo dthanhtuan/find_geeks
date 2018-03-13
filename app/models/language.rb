63f74728d23a3b8df68d65efd7b54ad8c4e78ba5
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-03-13 13:37:45 +0700

    Add validations

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,8 @@
 class Language < ApplicationRecord
   has_many :developer_languages, dependent: :destroy
   has_many :developers, through: :developer_languages
+
+  # NOTE: This is extra because there's validation on the db level.
+  #   But it's nice to have to show validation errors to the users.
+  validates :code, presence: true, uniqueness: true
 end
